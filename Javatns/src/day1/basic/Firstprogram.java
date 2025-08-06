0b02b54407d95354ed6e78b6175d909e7150ea42
MESHAM-MS <meshammelton@gmail.com>
2025-08-07 00:36:20 +0530

    Initial commit

---
+++ Javatns/src/day1/basic/Firstprogram.java
@@ -0,0 +1,8 @@
+package day1.basic;
+import java.util.LinkedList ; //all subpackages
+
+public class Firstprogram {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
